89bf79408879529a4e4c89fff2b626461fc2b6de
Francesco Benfenati <fbenfenati@km3net.de>
2021-12-20 18:26:33 +0100

    minor changes

--- agilent-grafana.sh
+++ agilent-grafana.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-script_dir=
+script_dir="/home/tdcmon/Agilent-53230A-remote-control"
 
 mkdir $script_dir/data
 mkdir $script_dir/logs
 
 
 
-/usr/bin/python3 $script_dir/pyvisa_agilent.py
-sleep 1
-FILENAME=$script_dir/data/*
-echo Copied $FILENAME
-sleep 1
-/usr/bin/python3 $script_dir/csv-to-influxdb_UTC_RP.py --dbname pps_monitoring --create --user km3net --password pyrosoma --server 172.16.65.46:8087 --input $FILENAME --fieldcolumns delay(s) -tc time -tf "%Y-%m-%d %H:%M:%S"
-sleep 1
-rm -f $FILENAME
-echo Removed csv
+/usr/bin/python3 $script_dir/pyvisa_agilent.py &
+
+inotifywait --monitor -r -e close_write --format "%f" $script_dir/data | while read FILENAME
+do
+    echo Copied $FILENAME
+    /usr/bin/python3 $script_dir/csv-to-influxdb_UTC_RP.py --dbname pps_monitoring --create --user km3net --password pyrosoma --server 172.16.65.46:8087 --input $script_dir/data/$FILENAME --fieldcolumns delay_s -tc time -tf "%Y-%m-%d %H:%M:%S"
+    sleep 1
+    rm -f $script_dir/data/$FILENAME
+    echo Removed csv
+done
