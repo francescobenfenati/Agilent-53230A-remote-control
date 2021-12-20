2621a5b46d6fdb5e0a9cb746041abb8af935b875
Francesco Benfenati <fbenfenati@km3net.de>
2021-12-20 14:17:55 +0100

    minor changes

--- agilent-grafana.sh
+++ agilent-grafana.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-script_dir=
-script_path=
-file_path=""
 
-/usr/bin/python3 $script_path
+script_dir=
+
+mkdir $script_dir/data
+mkdir $script_dir/logs
+
+
+
+/usr/bin/python3 $script_dir/pyvisa_agilent.py
 sleep 1
-FILENAME=$file_path/*
+FILENAME=$script_dir/data/*
 echo Copied $FILENAME
 sleep 1
 /usr/bin/python3 $script_dir/csv-to-influxdb_UTC_RP.py --dbname pps_monitoring --create --user km3net --password pyrosoma --server 172.16.65.46:8087 --input $FILENAME --fieldcolumns delay(s) -tc time -tf "%Y-%m-%d %H:%M:%S"
